67b6407e1b1fb346f49e5591eddb8499405a4188
protodomemusic <hello@protodome.com>
2021-05-01 23:19:27 +0100

    General cleanup
    Really this is so I can pull the latest version on a different machine.

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# This should really be ignored on commit but,
+# as there's no documentation for wavexe just
+# yet, it's helpful to see how this should be
+# used in conjuction with the mmml compiler.
+./mmml-compiler -f input.mmml -t wavexe -c 5
 gcc wavexe.c -o wavexe
 ./wavexe
 afplay output.wav
