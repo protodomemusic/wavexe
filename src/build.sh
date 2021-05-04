671efc19fc8ebd008f6caf8e98a2de9812789625
protodomemusic <hello@protodome.com>
2021-05-04 01:06:39 +0100

    Sampler channel, tie support, and sweep.
    All in the commit title, plus some general cleanup.

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,7 @@
 # as there's no documentation for wavexe just
 # yet, it's helpful to see how this should be
 # used in conjuction with the mmml compiler.
-./mmml-compiler -f input.mmml -t wavexe -c 5
+./mmml-compiler -f input.mmml -t wavexe -c 8
 gcc wavexe.c -o wavexe
 ./wavexe
 afplay output.wav
