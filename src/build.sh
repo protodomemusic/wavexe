809577958142c1ac3157a247554b92aed330a207
protodomemusic <hello@protodome.com>
2021-05-04 19:29:28 +0100

    Cleanup and compile definitions
    Experimenting with different methods of outputting audio data. To keep filesizes to a minimum, this is altered via definitions at compilation.

--- src/build.sh
+++ src/build.sh
@@ -2,8 +2,12 @@
 # This should really be ignored on commit but,
 # as there's no documentation for wavexe just
 # yet, it's helpful to see how this should be
-# used in conjuction with the mmml compiler.
+# used in conjunction with the mmml compiler.
+
+rm wavexe
 ./mmml-compiler -f input.mmml -t wavexe -c 8
-gcc wavexe.c -o wavexe
-./wavexe
-afplay output.wav
+gcc wavexe.c -o wavexe-big
+upx -9 -o wavexe wavexe-big # compress w/ upx for tiny files - can be commented out
+rm wavexe-big
+./wavexe | play -t raw -e unsigned-integer -b 8 -c 1 -r 44100 - # play with sox (MacOS)
+# ./wavexe | aplay -r 44100 # play with aplay (linux)
